39c7de5d2ae3b49978e96e2fc9e55753cc194b3a
yeyoung <91828757+yeyounglim@users.noreply.github.com>
2024-01-24 22:31:04 +0900

    Title: 중앙값 계산 2, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240124/중앙값 계산 2/get-median-2.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        int[] arr = new int[input];
+
+        for (int i = 0; i < input; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int[] result;
+        int[] midArr = new int[arr.length / 2 + 1];
+
+        int cnt = 0;
+        for (int i = 1; i < input + 1; i++) {
+            result = Arrays.copyOf(arr, i);
+            if (i % 2 != 0) {
+                Arrays.sort(result);
+                midArr[cnt] = result[i / 2];
+                cnt++;
+            }
+        }
+
+        for (int i = 0; i < midArr.length; i++) {
+            System.out.print(midArr[i] + " ");
+        }
+    }
+}
