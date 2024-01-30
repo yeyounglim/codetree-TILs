6c985e31d9392b2de936860a5d7548aa33ca1379
yeyoung <91828757+yeyounglim@users.noreply.github.com>
2024-01-30 19:19:07 +0900

    Title: 10진수로 변환하기, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240130/10진수로 변환하기/convert-to-decimal.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.next();
+        System.out.println(Integer.parseInt(input, 2));
+    }
+}
