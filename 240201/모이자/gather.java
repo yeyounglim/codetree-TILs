ce034276565553ff9c9b78d8e23b64b8ed36e83c
yeyoung <91828757+yeyounglim@users.noreply.github.com>
2024-02-01 23:02:40 +0900

    Update gather.java

--- 240201/모이자/gather.java
+++ 240201/모이자/gather.java
@@ -25,3 +25,27 @@ public class Main {
         System.out.println(Arrays.stream(sum).min().orElse(0));
     }
 }
+/* 정답
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        int[] arr = new int[input];
+        int[] sum = new int[input];
+
+        for (int i = 0; i < input; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < input; i++) {
+            sum[i] = 0;
+            for (int j = 0; j < input; j++) {
+                sum[i] += arr[j] * Math.abs(j - i);
+            }
+        }
+        System.out.println(Arrays.stream(sum).min().orElse(0));
+    }
+}
+*/
