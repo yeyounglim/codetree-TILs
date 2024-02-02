9f5df2f13e82a01298fcaa2d3cb9a10224e353f7
yeyoung <91828757+yeyounglim@users.noreply.github.com>
2024-02-02 15:29:09 +0900

    Title: 블럭쌓는 명령2, Time: 340ms, Memory: 15MB, Status: Passed - CodeTree

--- 240202/블럭쌓는 명령2/block-stacking-commands2.java
+++ 240202/블럭쌓는 명령2/block-stacking-commands2.java
@@ -10,7 +10,8 @@ public class Main {
         for (int i = 0; i < k; i++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
-            for (int j = a; j <= b; j++) {
+
+            for (int j = a - 1; j < b; j++) {
                 arr[j]++;
             }
         }
