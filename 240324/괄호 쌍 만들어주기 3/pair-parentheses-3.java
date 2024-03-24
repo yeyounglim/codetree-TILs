897fc866da5a909f93011787e6105fe116960a7d
yeyoung <91828757+yeyounglim@users.noreply.github.com>
2024-03-24 23:50:16 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240324/괄호 쌍 만들어주기 3/pair-parentheses-3.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int cnt = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                for (int j = i + 1; j < s.length(); j++) {
+                    if (s.charAt(j) == ')') {
+                        cnt++;
+                    }
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
