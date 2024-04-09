1313e27d1abe164f2065fd754009cb2bf025a27d
yeyoung <91828757+yeyounglim@users.noreply.github.com>
2024-04-09 23:56:31 +0900

    Title: 입력받아 계산, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240409/입력받아 계산/input-calculate.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        // 추가 코드 작성
+        int a = sc.nextInt();
+        System.out.println(a + 2);
+
+    }
+}
